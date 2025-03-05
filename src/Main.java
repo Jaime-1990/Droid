05cf73be880a47572214be20885598f1707fcf74
Sala315 <jjaimedavid@gmail.com>
2025-03-04 19:43:46 -0500

    xr10

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //String DroidName = "Codey";
-        Droid myDroid = new Droid("Codey");
+        Droid myDroid = new Droid("XR10");
         System.out.println(myDroid);
         myDroid.performTask("cleaning out my closet...");
     }
