b98576f445055e814af79424f3445cc034fefd7c
Jaime-1990 <jjaimedavid@gmail.com>
2025-03-04 19:40:02 -0500

    Add files via upload

--- src/Droid.java
+++ src/Droid.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Droid {
+    //instance field
+
+    int batteryLevel;
+    String name;
+    // constructor method
+    public Droid(String droidName) {
+        name = droidName;
+        batteryLevel = 100;
+    }
+   public String toString() {
+        return "Hello my name is Jaime" + name;
+   }
+
+   public void energyReport(){
+        System.out.println("Current battery level: " + batteryLevel + "%,");
+   }
+    public void performTask(String task) {
+        System.out.println(name + "is performing: " + task);
+        batteryLevel -= 10;
+        energyReport();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        //String DroidName = "Codey";
+        Droid myDroid = new Droid("Codey");
+        System.out.println(myDroid);
+        myDroid.performTask("cleaning out my closet...");
+    }
 }
